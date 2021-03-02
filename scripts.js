d4be699ad549cd4a4d1cf25d23e94300de8389ce
L3p1989 <phogan110@live.com>
2021-03-02 15:00:33 -0700

    turned unchanging variables from let to const. Created divContainer div.square-container to put div.square's in. Inserted divContainer into body. changed div.square insert to insert into divContainer

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
 //create button
-let squareBtn = document.createElement("button");
+const squareBtn = document.createElement("button");
 //Create text "Add Square"
-let btnText = document.createTextNode("Add Square");
+const btnText = document.createTextNode("Add Square");
 //Add btnText to squareBtn
 squareBtn.appendChild(btnText);
 //Grab body
-let body = document.body;
+const body = document.body;
 //Insert square4Btn into body
 body.appendChild(squareBtn);
+//Create a container
+const divContainer = document.createElement('div');
+//Give divContainer className "square-container"
+divContainer.className = "square-container"
+//insert divContainer into body
+body.appendChild(divContainer);
 //Create click event listener for squareBtn
 squareBtn.addEventListener('click', () => {
     //Create div
-    let squareDiv = document.createElement('div');
+    const squareDiv = document.createElement('div');
     //Give div className square
     squareDiv.className = "square";
     //Insert squareDiv into body
-    body.appendChild(squareDiv);
+    divContainer.appendChild(squareDiv);
 })
