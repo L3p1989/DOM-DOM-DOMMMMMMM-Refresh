d74fa719ce7c3b3d9a1808bd439d05bc8aab3ac6
L3p1989 <phogan110@live.com>
2021-03-02 14:34:49 -0700

    created click event listener for squareBtn that creates a div.square every time it's clicked

--- scripts.js
+++ scripts.js
@@ -8,3 +8,12 @@ squareBtn.appendChild(btnText);
 let body = document.body;
 //Insert square4Btn into body
 body.appendChild(squareBtn);
+//Create click event listener for squareBtn
+squareBtn.addEventListener('click', () => {
+    //Create div
+    let squareDiv = document.createElement('div');
+    //Give div className square
+    squareDiv.className = "square";
+    //Insert squareDiv into body
+    body.appendChild(squareDiv);
+})
